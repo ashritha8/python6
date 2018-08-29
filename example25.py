1c270ea0cdd045ab2b16046ec498347b862ae39f
ashritha8 <32218727+ashritha8@users.noreply.github.com>
2018-08-29 14:42:32 +0530

    Add files via upload

---
+++ example25.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+img=cv2.imread('lena.jpg')
+
+height,width=img.shape[:2]
+
+start_row,start_col=int(height*.25),int(width*.25)
+
+end_row,end_col=int(height*.75),int(width*.75)
+
+cropped=img[start_row:end_row,start_col:end_col]
+
+cv2.imshow('Original',img)
+cv2.waitKey(0)
+
+cv2.imshow('Cropped',cropped)
+cv2.waitKey()
+
+cv2.destroyAllWindows()
